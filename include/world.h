25bac40f2f492090ffa98aa77c305b66c34a4e7a
alex <pcranaway@tuta.io>
2023-05-14 22:05:12 +0300

    add world and location classes

--- include/world.h
+++ include/world.h
@@ -1,3 +1,4 @@
+#include <memory>
 #include <string>
 
 enum WorldType
@@ -10,5 +11,29 @@ enum WorldType
 class World
 {
 public:
-    World(std::string name)
-}
+    std::string name;
+    WorldType type;
+
+public:
+    World(std::string name, WorldType type)
+        : name { name }
+        , type { type }
+    {
+    }
+};
+
+class Location
+{
+public:
+    double x, y, z;
+    std::shared_ptr<World> world;
+
+public:
+    Location(double x, double y, double z, std::shared_ptr<World> world)
+        : x { x }
+        , y { y }
+        , z { z }
+        , world { world }
+    {
+    }
+};
