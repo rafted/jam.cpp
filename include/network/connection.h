1c02bb32b69a504e806fb77a5c1a5e67c46c2a62
alex <pcranaway@tuta.io>
2023-05-15 21:26:05 +0300

    add connection state based on the minecraft protocol

--- include/network/connection.h
+++ include/network/connection.h
@@ -3,6 +3,15 @@
 #include <memory>
 #include <uvw.hpp>
 
+enum ConnectionState
+{
+    Handshake,
+    Status,
+    Login,
+    Play,
+    Closed
+};
+
 class Connection
 {
 private:
@@ -11,8 +20,11 @@ private:
 public:
     TcpHandle tcp_handle;
 
+    ConnectionState state;
+
     Connection(TcpHandle tcp_handle)
         : tcp_handle(tcp_handle)
+        , state(ConnectionState::Handshake)
     {
     }
 };
