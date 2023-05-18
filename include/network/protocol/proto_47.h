ff89c14f36b64cfac06cd106c2aa44abe093293b
alex <pcranaway@tuta.io>
2023-05-18 23:51:42 +0300

    add base for implementing packets

---
+++ include/network/protocol/proto_47.h
@@ -0,0 +1,58 @@
+#include "network/protocol/packet.h"
+
+namespace proto_47
+{
+
+    namespace handshaking
+    {
+
+        namespace clientbound
+        {
+        }
+
+        namespace serverbound
+        {
+        }
+
+    }
+
+    namespace play
+    {
+
+        namespace clientbound
+        {
+        }
+
+        namespace serverbound
+        {
+        }
+
+    }
+
+    namespace status
+    {
+
+        namespace clientbound
+        {
+        }
+
+        namespace serverbound
+        {
+        }
+
+    }
+
+    namespace login
+    {
+
+        namespace clientbound
+        {
+        }
+
+        namespace serverbound
+        {
+        }
+
+    }
+
+}
