ff711483dce24644f2c690a8e99ceed5f914cd04
alex <pcranaway@tuta.io>
2023-05-20 00:07:35 +0300

    add code for reading big endian numbers

---
+++ include/network/protocol/encoding/numbers.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdexcept>
+#include <vector>
+
+#include <stdexcept>
+#include <vector>
+
+template<typename T>
+T read_big_endian(std::vector<char> &data)
+{
+    static_assert(std::is_integral<T>::value, "T must be an integral type");
+
+    const std::size_t size = sizeof(T);
+
+    if (data.size() < size)
+    {
+        throw std::length_error("Not enough data to read");
+    }
+
+    T value = 0;
+
+    for (std::size_t i = 0; i < size; ++i)
+    {
+        value = (value << 8) | static_cast<unsigned char>(data[i]);
+    }
+
+    data.erase(data.begin(), data.begin() + size);
+
+    return value;
+}
