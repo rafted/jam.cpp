bc89b6dc38bb6b0eeb412be9d52bcccce2f19264
therealnv6 <devrawr@protonmail.com>
2023-05-21 21:02:04 +0200

    remove unused field and tidy up

--- include/network/protocol/packet.h
+++ include/network/protocol/packet.h
@@ -33,7 +33,6 @@ public:
     void emplace(std::function<void(PacketContainer)> decode)
     {
         static_assert(std::is_base_of_v<Packet, T>, "T must derive from Packet");
-        packets[typeid(T).hash_code()] = std::make_unique<T>();
         packet_decode[typeid(T).hash_code()] = decode;
     }
 
@@ -41,12 +40,9 @@ public:
     std::function<void(PacketContainer)> get() const
     {
         static_assert(std::is_base_of_v<Packet, T>, "T must derive from Packet");
-        auto it = packet_decode.find(typeid(T).hash_code());
-
-        return it->second;
+        return packet_decode.find(typeid(T).hash_code())->second;
     }
 
 private:
-    std::unordered_map<size_t, std::unique_ptr<Packet>> packets;
     std::unordered_map<size_t, std::function<void(PacketContainer container)>> packet_decode;
 };
