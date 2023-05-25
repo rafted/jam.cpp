5295ad4ad482f49914995ec2797ba0fd0ac1003e
alex <pcranaway@tuta.io>
2023-05-25 23:14:54 +0300

    oops, actually create the packet registry

--- include/network/server.h
+++ include/network/server.h
@@ -2,6 +2,7 @@
 
 #include "network/connection.h"
 #include "network/protocol/packet.h"
+#include "network/protocol/proto_47.h"
 #include <memory>
 #include <spdlog/spdlog.h>
 #include <string>
@@ -25,13 +26,13 @@ public:
     std::shared_ptr<uvw::loop> loop;
     std::shared_ptr<uvw::tcp_handle> tcp_handle;
 
-    // std::unique_ptr<PacketRegistry> packet_registry;
+    std::unique_ptr<PacketRegistry> packet_registry;
 
     Server(std::shared_ptr<uvw::loop> loop, Properties properties)
         : loop(loop)
         , properties(properties)
     {
-        // packet_registry = std::make_unique<PacketRegistry>(PacketRegistry());
+        packet_registry = std::make_unique<PacketRegistry>(proto_47::make_packet_registry());
     }
 
     void start();
